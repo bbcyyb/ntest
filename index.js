d74c938b4dc3307092461263e304912c02979199
Kevin.Y.Yu <bbcyyb@gmail.com>
2017-09-06 00:57:05 +0800

    Add all function.

--- index.js
+++ index.js
@@ -5,23 +5,68 @@
 'use strict';
 
 var _ = require('lodash'),
-    Promise = require('bluebird'),
-    di = require('di');
+    Promise = require('bluebird');
 
-console.log("hello world");
+var promiseCall = function() {
+    /* chainFunc */
+    this.chainFunc = function(fun) {
+        console.log("=====================>chainFunc...");
+        var counter = 0;
+        console.log(counter);
+        counter += 1;
+        Promise.resolve()
+            .then(function() {
+                console.log(counter);
+                counter += 1;
+                return counter;
+            })
+            .then(function(data) {
+                console.log(data);
+                data += 1;
+                return data;
+            })
+            .then(function(data) {
+                console.log(data);
+                return data;
+            });
+        console.log("<====================");
+        // if(fun){
+        //     fun();
+        // }
+    };
 
-Promise.resolve()
-    .then(function() {
-        console.log("11111");
-        return "22222"
-    })
-.then(function(data) {
-    console.log(data);
-    return '33333';
-})
-.then(function(data) {
-    console.log(data);
-    return '444444';
-});
+    this.allFunc = function() {
+        console.log("====================>allFunc...");
+        var parallelOne = function() {
+            return Promise.resolve()
+                .then(function() {
+                    return "One";
+                });
+        };
+        var parallelTwo = function() {
+            return Promise.resolve()
+                .then(function() {
+                    return "Two";
+                });
+        };
+        var parallelThree = function() {
+            return Promise.resolve()
+                .then(function() {
+                    return "Three";
+                });
+        };
 
-console.log("bye world");
+        Promise.all([parallelOne, parallelTwo, parallelThree])
+            .spread(function(resultOne, resultTwo, resultThree) {
+                console.log(resultOne());
+                console.log(resultTwo);
+                console.log(resultThree);
+            });
+        console.log("<===================");
+    };
+};
+
+var obj = new promiseCall();
+// obj.chainFunc();
+obj.allFunc();
+// obj.chainFunc(obj.chainFunc);
